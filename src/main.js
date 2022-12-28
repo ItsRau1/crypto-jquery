d0490ec6fb3b528ab0dfffbb6a1c9968a3f450a5
ItsRau1 <00001085823507sp@al.educacao.sp.gov.br>
2022-12-27 22:23:32 -0300

    att

--- src/main.js
+++ src/main.js
@@ -1,186 +1,53 @@
-$(document).ready(function() {
-    var chart = new CanvasJS.Chart("chartContainer", {
-        title:{
-            text: "Statistic",
-            horizontalAlign: 2,
-            fontSize: 21,
-            fontWeight: 500,
-            fontColor: "#1A1A1A",
-        },
-        axisX:{
-            gridColor: "transparent",
-            tickColor: "transparent",
-            lineColor: "transparent",
-            labelFontColor: "#9E9E9E",
-            labelFontSize: 12,
-            labelFontFamily: "Inter",
+jQuery.get("https://api.binance.com/api/v3/ticker/24hr", function(res){
+    const cots = res.filter(function (cot){
+        return cot.symbol.includes("USDT")
+    })
+    const filtered = [cots[0], cots[1], cots[5], cots[7]]
+    
+    var cardCoin = ''
 
-        },
-        axisY:{
-            gridColor: "transparent",
-            tickColor: "transparent",
-            lineColor: "transparent",
-            labelFontColor: "#9E9E9E",
-            labelFontSize: 12,
-            labelFontFamily: "Inter",
-            prefix: "$"
-         },
-		data: [              
-		{
-            type: "spline",
-            lineColor: "#6154F0",
-            backgroundColor:"transparent",
-            markerType: "none",
-            lineThickness: 2,
-			dataPoints: [
-				{ label: "Jan",  y: 20  },
-				{ label: "Mar", y: 50  },
-				{ label: "May", y: 30  },
-				{ label: "Jul",  y: 20  },
-				{ label: "Sep",  y: 25  },
-                { label: "Nov",  y: 40  },
-			]
-		}
-		]
-	});
-	chart.render();
+    $.each(filtered, function (key, item){
+        cardCoin = cardCoin + '<div class="card-coin">'
+        cardCoin = cardCoin +   '<div class="header-coin">'
+        cardCoin = cardCoin +       '<img src="./assets/icon-' + item.symbol +'.svg">'
+        cardCoin = cardCoin +       '<div class="header-side down">'
+        cardCoin = cardCoin +           '<img src="./assets/icon-arrow-dowm.svg" alt="">'
+        cardCoin = cardCoin +           '<p>' + item.priceChangePercent + '</p>'
+        cardCoin = cardCoin +       '</div>'
+        cardCoin = cardCoin +   '</div>'
+        cardCoin = cardCoin +   '<div class="footer-coin">'
+        cardCoin = cardCoin +       '<p class="price-coin">' + '$ '+ new Intl.NumberFormat().format(Number(item.lastPrice)) + '</p>'
+        cardCoin = cardCoin +       '<p class="name-coin">' + item.symbol.replace("USDT", "").replace("BTC", "Bitcoin").replace("LTC", "Litecoin").replace("ADA", "Cardano").replace("ETH", "Ethereum") + '</p>'
+        cardCoin = cardCoin +   '</div>'
+        cardCoin = cardCoin + '</div>'
+        console.log(item)
+    })
 
-    var miniChart1 = new CanvasJS.Chart("miniChartContainer1", {
-        axisX:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-        },
-        axisY:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-            margin: 0,
-         },
-		data: [              
-		{
-            type: "spline",
-            lineColor: "#6154F0",
-            backgroundColor:"transparent",
-            markerType: "none",
-            lineThickness: 2,
-			dataPoints: [
-				{ x: 1,  y: 20  },
-				{ x: 2, y: 50  },
-				{ x: 3, y: 5  },
-				{ x: 4,  y: 90  },
-				{ x: 5,  y: 25  },
-                { x: 6,  y: 40  },
-			]
-		}
-		]
-	});
-	miniChart1.render();
+    $("#main-resume-coins").html(cardCoin)
 
-    var miniChart2 = new CanvasJS.Chart("miniChartContainer2", {
-        axisX:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-        },
-        axisY:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-            margin: 0,
-         },
-		data: [              
-		{
-            type: "spline",
-            lineColor: "#FABC1E",
-            backgroundColor:"transparent",
-            markerType: "none",
-            lineThickness: 2,
-			dataPoints: [
-				{ x: 1,  y: 20  },
-				{ x: 2, y: 50  },
-				{ x: 3, y: 5  },
-				{ x: 4,  y: 90  },
-				{ x: 5,  y: 25  },
-                { x: 6,  y: 40  },
-			]
-		}
-		]
-	});
-	miniChart2.render();
+    var itemLiveMarket = ''
 
-    var miniChart3 = new CanvasJS.Chart("miniChartContainer3", {
-        axisX:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-        },
-        axisY:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-            margin: 0,
-         },
-		data: [              
-		{
-            type: "spline",
-            lineColor: "#3855EC",
-            backgroundColor:"transparent",
-            markerType: "none",
-            lineThickness: 2,
-			dataPoints: [
-				{ x: 1,  y: 20  },
-				{ x: 2, y: 50  },
-				{ x: 3, y: 5  },
-				{ x: 4,  y: 90  },
-				{ x: 5,  y: 25  },
-                { x: 6,  y: 40  },
-			]
-		}
-		]
-	});
-	miniChart3.render();
+    $.each(filtered, function (key, item){
+        itemLiveMarket = itemLiveMarket + '<div class="item-live-market">'
+        itemLiveMarket = itemLiveMarket +   '<div class="item-live-head">'
+        itemLiveMarket = itemLiveMarket +       '<img src="./assets/icon-' + item.symbol +'.svg">'
+        itemLiveMarket = itemLiveMarket +       '<div class="item-live-market-title-box">'
+        itemLiveMarket = itemLiveMarket +           '<p class="item-live-market-name-coin">' + item.symbol.replace("USDT", "").replace("BTC", "Bitcoin").replace("LTC", "Litecoin").replace("ADA", "Cardano").replace("ETH", "Ethereum") + '</p>'
+        itemLiveMarket = itemLiveMarket +           '<p>' + item.symbol.replace("USDT", "") + '/ USDT </p>'
+        itemLiveMarket = itemLiveMarket +       '</div>'
+        itemLiveMarket = itemLiveMarket +   '</div>'
+        itemLiveMarket = itemLiveMarket +   '<div class="item-live-market-change-box">'
+        itemLiveMarket = itemLiveMarket +       '<p class="item-live-market-subtitle">Change</p>'
+        itemLiveMarket = itemLiveMarket +       '<p>' + item.priceChangePercent + '</p>'
+        itemLiveMarket = itemLiveMarket +   '</div>'
+        itemLiveMarket = itemLiveMarket +   '<div class="item-live-market-price-box">'        
+        itemLiveMarket = itemLiveMarket +       '<p class="item-live-market-subtitle">Price</p>'
+        itemLiveMarket = itemLiveMarket +       '<p>' + new Intl.NumberFormat().format(Number(item.lastPrice)) + ' USD</p>'
+        itemLiveMarket = itemLiveMarket +   '</div>'
+        itemLiveMarket = itemLiveMarket + '</div>'
+    })
 
-    var miniChart4 = new CanvasJS.Chart("miniChartContainer4", {
-        axisX:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-        },
-        axisY:{
-            gridColor: "none",
-            tickColor: "none",
-            lineColor: "transparent",
-            labelFontColor: "none",
-            margin: 0,
-         },
-		data: [              
-		{
-            type: "spline",
-            lineColor: "#1ECB4F",
-            backgroundColor:"transparent",
-            markerType: "none",
-            lineThickness: 2,
-			dataPoints: [
-				{ x: 1,  y: 20  },
-				{ x: 2, y: 50  },
-				{ x: 3, y: 5  },
-				{ x: 4,  y: 90  },
-				{ x: 5,  y: 25  },
-                { x: 6,  y: 40  },
-			]
-		}
-		]
-	});
-	miniChart4.render();
+    $("#main-live-market").html(itemLiveMarket)
 })
 
-
-
+// .match(/^-?\d+(?:\.\d{0,2})?/)[0]
